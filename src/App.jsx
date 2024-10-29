3d438fd111069d78069000543af9943f39de8d15
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-29 17:22:36 +0200

    Make navigation responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navigation from "./components/Navigation/Navigation";
 
 export default function App() {
     return (
-        <main className="w-full absolute min-h-screen bg-dark-500 font-serif overflow-x-hidden">
+        <main className="w-full absolute min-h-screen bg-dark-500 font-main overflow-x-hidden">
             <Navigation />
         </main>
     )
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,20 +11,20 @@ export default function Navigation() {
       <BigMenu isOpen={isOpen} setIsOpen={setIsOpen}/>
       <div className="w-full fixed z-40 flex items-center py-5">
         {/* Logo container */}
-        <div className="flex items-center justify-center w-80">
+        <div className="flex items-center justify-start sm:justify-center w-80 ps-4 sm:ps-0">
           <img
             src="/images/estatevision-logo.png"
             className="max-w-48 max-h-16"
           />
         </div>
         {/* Buttons Container */}
-        <div className="grow flex justify-center items-center">
-          <NavigationButton to="/" title="Home" />
-          <NavigationButton to="/" title="Projects" />
-          <NavigationButton to="/" title="Pricing" />
+        <div className="grow shrink-0 hidden sm:flex justify-center items-center">
+          <NavigationButton to="/" title="Начало" />
+          <NavigationButton to="/" title="Проекти" />
+          <NavigationButton to="/" title="Цени" />
         </div>
         {/* Toggle Button Container */}
-        <div className="w-80 flex items-center justify-center">
+        <div className="w-80 flex items-center justify-end sm:justify-center pe-4 sm:pe-0">
           <HamburgerButton setIsOpen={setIsOpen}/>
         </div>
       </div>
@@ -37,14 +37,14 @@ function BigMenu({ isOpen, setIsOpen }) {
     <>
       <nav className={`fixed top-0 ${isOpen ? "left-0" : "left-full"} w-full min-h-screen z-50 bg-white transition-all duration-1000 ease-out`}>
         <header className="flex items-center py-10">
-          <div className="w-80 flex items-center justify-center">
+          <div className="w-80 flex items-center lg:justify-center justify-start ps-8 lg:ps-0">
             <img
               src="/images/estatevision-logo.png"
               className="max-w-52 max-h-16"
             />
           </div>
           <div className="grow"></div>
-          <div className="w-80 flex items-center justify-center">
+          <div className="w-80 flex items-center lg:justify-center justify-end pe-8 lg:pe-0">
             <svg
               width="25"
               height="25"
@@ -65,20 +65,20 @@ function BigMenu({ isOpen, setIsOpen }) {
         </header>
 
         {/* Buttons */}
-        <div className="mt-24 flex flex-wrap px-24 gap-x-20 gap-y-7">
-            <div className="w-96 flex flex-col">
-                <Link className="text-6xl mb-7 text-dark-500">Home</Link>
-                <Link className="text-6xl mb-7">Projects</Link>
-                <Link className="text-6xl">Pricing</Link>
+        <div className="lg:mt-20 mt-4 flex flex-wrap sm:px-24 px-4 gap-x-20 gap-y-7">
+            <div className="lg:w-96 w-full flex flex-col gap-y-7">
+                <BigNavigationButton title="Начало" to="/"/>
+                <BigNavigationButton title="Проекти" to="/"/>
+                <BigNavigationButton title="Цени" to="/"/>
             </div>
-            <div className="w-96 flex flex-col">
-                <Link className="text-6xl mb-7">About us</Link>
-                <Link className="text-6xl">Contact us</Link>
+            <div className="lg:w-96 w-full flex flex-col gap-y-7">
+                <BigNavigationButton title="За нас" to="/"/>
+                <BigNavigationButton title="Свържете се" to="/"/>
             </div>
         </div>
 
         {/* Information */}
-        <div className="mt-32 flex">
+        <div className="lg:mt-32 mt-8 lg:flex flex-wrap hidden">
             <div className="flex items-center ms-24 me-8">
                 <MdOutlinePhone className="text-2xl"/>
                 <p className="text-xl ms-2">+359 88 888 888</p>
@@ -91,6 +91,17 @@ function BigMenu({ isOpen, setIsOpen }) {
       </nav>
     </>
   );
+}
+
+function BigNavigationButton({ to, title }) {
+    return (
+        <>
+            <Link className="text-2xl lg:text-6xl sm:text-4xl text-dark-500 hover:text-yellow-700 relative transition-all duration-300" to={to}>
+                {title}
+                <span className="absolute right-0 inline-block lg:hidden text-2xl sm:text-4xl">&rarr;</span>
+            </Link>
+        </>
+    )
 }
 
 function NavigationButton({ to, title }) {
