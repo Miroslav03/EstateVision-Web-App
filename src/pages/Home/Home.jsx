469198114a5f29ac80cdccdc2c3fc47c55e2f635
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-30 00:17:02 +0200

    Create new hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import HeroSection from "../../components/Home/HeroSection";
+import HeroSection2 from "../../components/Home/HeroSection2";
 import Navigation from "../../components/Navigation/Navigation";
 
 export default function Home() {
     return (
         <main className="w-full overflow-x-hidden font-main min-h-screen bg-dark-500">
             <Navigation />
-            <HeroSection />
+            <HeroSection2 />
         </main>
     );
 }
