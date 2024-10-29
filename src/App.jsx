a56d3e8465a69de6de12808972c4310bef66a996
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-29 22:37:26 +0200

    Import routes and create route for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
 export default function App() {
     return (
-        <>
-            <Home />
-        </>
+        <Routes>
+            <Route path="/" Component={Home}/>
+        </Routes>
     );
 }
-/* import Navigation from "./components/Navigation/Navigation";
-
-export default function App() {
-    return (
-        <main className="w-full absolute min-h-screen bg-dark-500 font-main overflow-x-hidden">
-            <Navigation />
-        </main>
-    )
-} */
