7d1265ca330ee2d68a9224641a65343ba0a36b8f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-29 22:35:30 +0200

    Import navigation and fix hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import HeroSection from "../../components/Home/HeroSection";
+import Navigation from "../../components/Navigation/Navigation";
 
 export default function Home() {
     return (
-        <main className="w-full overflow-x-hidden">
+        <main className="w-full overflow-x-hidden font-main min-h-screen bg-dark-500">
+            <Navigation />
             <HeroSection />
         </main>
     );
