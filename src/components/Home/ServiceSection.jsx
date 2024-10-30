f69c0c8317630752c10cccbc34893fd6519473f7
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-30 16:39:28 +0200

    Pushing

--- src/components/Home/ServiceSection.jsx
+++ src/components/Home/ServiceSection.jsx
@@ -11,19 +11,19 @@ export default function ServiceSection() {
     ];
 
     return (
-        <div className="h-[110vh] flex flex-col items-center bg-gradient-to-r from-dark-700 to-dark-500 ">
-            <div className="w-full max-w-[90%] lg:max-w-[90%] flex flex-col items-start px-6">
+        <div className="h-[110vh] flex flex-col items-center bg-gradient-to-r from-dark-700 to-dark-500">
+            <div className="w-full max-w-[90%] lg:max-w-[90%] flex flex-col items-start px-4 sm:px-6">
                 <div className="border-b-8 border-yellow-500">
-                    <h2 className="text-6xl font-semibold mt-16 text-white mb-4">
+                    <h2 className="text-4xl sm:text-6xl font-semibold mt-16 text-white mb-4">
                         Услуги
                     </h2>
                 </div>
-                <div className="flex justify-between items-center w-full mt-2">
-                    <p className="text-gray-500 text-xl">
+                <div className="flex sm:flex-row flex-col sm:gap-0 gap-6 justify-between items-start sm:items-center w-full mt-2">
+                    <p className="text-gray-500 text-md sm:text-xl">
                         Разгледай и избери услугата за теб
                     </p>
                     <Link
-                        className="text-white font-main px-5 py-[0.5rem] text-lg transition duration-300 mx-[1.5rem] sm:mx-0 relative overflow-hidden flex items-center justify-center group bg-yellow-500 hover:ring-2 hover:ring-yellow-500"
+                        className="text-white font-main px-5 py-[0.5rem] text-lg transition duration-300 sm:mx-0 relative overflow-hidden flex items-center justify-center group bg-yellow-500 hover:ring-2 hover:ring-yellow-500"
                         href=""
                     >
                         <p className="z-20 group-hover:text-dark-500">
@@ -37,11 +37,15 @@ export default function ServiceSection() {
                 </div>
             </div>
 
-            <div className="flex gap-6 w-full max-w-[90%] lg:max-w-[90%] transition-all duration-300 mt-8 ">
+            <div className="flex sm:flex-row flex-col gap-6 w-full max-w-[90%] lg:max-w-[90%] transition-all duration-300 mt-8">
                 {["k.png", "3dmodel.jpg", "drone1.jpg"].map((img, index) => (
                     <div
                         key={index}
-                        className={`relative bg-white h-[75vh] shadow-lg overflow-hidden transform transition-all duration-300 hover:border-l-8 border-yellow-500 ${
+                        className={`relative bg-white h-[75vh] shadow-lg overflow-hidden transform transition-all duration-300 ${
+                            hoveredIndex === index
+                                ? "sm:border-l-8 border-yellow-500"
+                                : "border-l-4 sm:border-l-0 border-yellow-500"
+                        } ${
                             hoveredIndex === index
                                 ? index === 1
                                     ? "flex-[1.2]"
@@ -63,16 +67,16 @@ export default function ServiceSection() {
                         <div className="absolute inset-0 bg-gradient-to-t from-dark-700 to-transparent opacity-40 transition-opacity duration-300"></div>
 
                         <div
-                            className={`absolute bottom-4 left-4 text-white mb-8 ml-5 transition-all duration-300 transform ${
-                                hoveredIndex === index
+                            className={`absolute bottom-4 left-4 text-white mb-2 sm:mb-8 ml:2 sm:ml-5 transition-all duration-300 transform ${
+                                hoveredIndex === index || !hoveredIndex
                                     ? "translate-x-0 opacity-100"
-                                    : "translate-x-full opacity-0"
+                                    : "translate-x-full opacity-0 sm:translate-x-0 sm:opacity-100"
                             }`}
                         >
-                            <p className="text-3xl font-semibold">
+                            <p className="text-xl sm:text-2xl font-semibold">
                                 {hoverText[index]}
                             </p>
-                            <div className="h-1 bg-yellow-500 mt-2 w-full"></div>
+                            <div className="h-[0.1rem] sm:h-1 bg-yellow-500 mt-2 w-[100%] sm:w-full"></div>
                         </div>
                     </div>
                 ))}
