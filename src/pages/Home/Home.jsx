304d96bf302002aaa8d4523c3abe26244533c848
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-31 14:23:31 +0200

    Add section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import HeroSection from "../../components/Home/HeroSection";
 import HeroSection2 from "../../components/Home/HeroSection2";
 import ServiceSection from "../../components/Home/ServiceSection";
 import Navigation from "../../components/Navigation/Navigation";
+import LiveDemoSection from "../../components/Home/LiveDemoSection";
 
 export default function Home() {
     return (
         <main className="w-full overflow-x-hidden font-main min-h-screen bg-dark-500">
             <Navigation />
             <HeroSection />
+            <LiveDemoSection />
             <ServiceSection />
         </main>
     );
