ceab662ffd92665a9052c684e1401d5a6d49b15e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-07 00:04:24 +0200

    Create smtp.js

---
+++ src/smtp.js
@@ -0,0 +1,6 @@
+import { createClient } from "smtpexpress"
+
+export const smtpexpressClient = createClient({
+  projectId: "sm0pid-u9UbzddyoFEzsRCywR_0sfFq_",
+  projectSecret: "84e21e026b13572dad1800dc3650da89a783a68c6521dbc6f8"
+});
