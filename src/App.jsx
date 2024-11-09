78a56c5cb8a1b0f26b60e0a03db72ac81605171d
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-11-09 18:04:34 +0200

    Fix lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
     }
 
     return (
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={null}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/services" element={<Services />} />
